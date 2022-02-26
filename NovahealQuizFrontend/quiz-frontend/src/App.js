6f893a91ae99037dcbb6147bc7eac84a1e71710e
Jonas Knapstein <jonas.knapstein@gmail.com>
2022-02-26 10:23:18 +0100

    Hintergrund für die Homepage

--- NovahealQuizFrontend/quiz-frontend/src/App.js
+++ NovahealQuizFrontend/quiz-frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Center from './components/Center';
 
 function App() {
   return (
     <div className="App">
-      
+        <Header></Header>
+        <Center></Center>
+        <Footer></Footer>
     </div>
   );
 }
---
+++ NovahealQuizFrontend/quiz-frontend/src/components/Center.js
@@ -0,0 +1,7 @@
+import './Center.css'
+
+function Center(){
+    return <div className='Center'></div>
+}
+
+export default Center;
